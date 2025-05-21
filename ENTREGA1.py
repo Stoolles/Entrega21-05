2b3544a7919d62d28533d346249a3128ac787a10
Stoolles <156022880+Stoolles@users.noreply.github.com>
2025-05-21 18:47:25 -0300

    Update ENTREGA1.py

--- ENTREGA1.py
+++ ENTREGA1.py
@@ -1,5 +1,10 @@
+#Escreva um programa que crie uma lista com números aleatórios e a imprima na
+#tela
+
+
 import random
 
 lista = [random.randint(1, 100) for _ in range(10)]
 print("Lista aleatória:", lista)
 
+#Uso basico da lista com o uso do import random
